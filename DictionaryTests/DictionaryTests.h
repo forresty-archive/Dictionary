7491c2c838bdea493b0a3a2f1597968107eb6a79
Forrest Ye <afu@forresty.com>
2011-11-18 10:58:26 +0800

    Initial Commit

---
+++ DictionaryTests/DictionaryTests.h
@@ -0,0 +1,13 @@
+//
+//  DictionaryTests.h
+//  DictionaryTests
+//
+//  Created by Feng Ye on 11/18/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DictionaryTests : SenTestCase
+
+@end
---
+++ DictionaryTests/DictionaryTests.m
@@ -0,0 +1,32 @@
+//
+//  DictionaryTests.m
+//  DictionaryTests
+//
+//  Created by Feng Ye on 11/18/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "DictionaryTests.h"
+
+@implementation DictionaryTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DictionaryTests");
+}
+
+@end
