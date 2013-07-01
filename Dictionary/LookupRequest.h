6bfd8914eb608c1e78f3d0ab079ede8e3b6b8644
Forrest Ye <afu@forresty.com>
2013-07-01 11:18:13 +0800

    DictionaryLookupProgressState enum definition

--- Dictionary/LookupRequest.h
+++ Dictionary/LookupRequest.h
@@ -10,6 +10,26 @@
 
 typedef void(^DictionaryLookupPartialResult)(NSArray *partialResults);
 
+typedef NS_ENUM(NSUInteger, DictionaryLookupProgressState) {
+  // start looking up completions, but no results yet
+  DictionaryLookupProgressStateLookingUpCompletionsButNoResultYet,
+
+  // still working on term completions, some results are found
+  DictionaryLookupProgressStateHasPartialResults,
+
+  // found no completions, looking up guesses, but no results yet
+  DictionaryLookupProgressStateFoundNoCompletionsLookingUpGuessesButNoResultsYet,
+
+  // finished, completions found
+  DictionaryLookupProgressStateFinishedWithCompletions,
+
+  // finished, can not find completions, but found guesses
+  DictionaryLookupProgressStateFinishedWithGuesses,
+
+  // finished, but no completions or guesses were found
+  DictionaryLookupProgressStateFinishedWithNoResultsAtAll
+};
+
 
 
 @interface LookupRequest : NSObject
