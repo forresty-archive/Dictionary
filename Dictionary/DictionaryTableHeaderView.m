c230e7e8d36d45160d6ed7c5d6e014e94a352787
Forrest Ye <afu@forresty.com>
2013-07-02 01:58:28 +0800

    oh my flightweight

--- Dictionary/DictionaryTableHeaderView.m
+++ Dictionary/DictionaryTableHeaderView.m
@@ -12,17 +12,13 @@
 
 @interface DictionaryTableHeaderView ()
 
-@property UILabel *label;
-
 @end
 
 @implementation DictionaryTableHeaderView
 
 
 + (instancetype)viewWithText:(NSString *)text {
-  DictionaryTableHeaderView *view = [self sharedInstance];
-
-  view.label.text = text;
+  DictionaryTableHeaderView *view = [self sharedInstanceWithText:text];
 
   return view;
 }
@@ -31,34 +27,43 @@
 # pragma mark - private
 
 
-+ (instancetype)sharedInstance {
-  static DictionaryTableHeaderView *_instance = nil;
++ (instancetype)sharedInstanceWithText:(NSString *)text {
+  static NSMutableDictionary* _instances = nil;
+
 
   static dispatch_once_t onceToken;
 
   dispatch_once(&onceToken, ^{
-    _instance = [[self alloc] init];
+    _instances = [@{} mutableCopy];
   });
 
-  return _instance;
+  if (!_instances[text]) {
+    DictionaryTableHeaderView *view = [[self alloc] initWithText:text];
+    _instances[text] = view;
+  }
+
+  return _instances[text];
 }
 
-- (instancetype)init {
+
+- (instancetype)initWithText:(NSString *)text {
   self = [super initWithFrame:CGRectMake(0, 0, 320, 30)];
 
   if (self) {
     self.backgroundColor = DICTIONARY_BASIC_TEXT_COLOR;
     self.alpha = 0.9;
 
-    _label = [[UILabel alloc] initWithFrame:CGRectMake(10, 0, 310, 30)];
-    self.label.backgroundColor = [UIColor clearColor];
-    self.label.textColor = [UIColor whiteColor];
-    self.label.font = [UIFont fontWithName:@"Helvetica-Bold" size:16];
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(10, 0, 310, 30)];
+    label.backgroundColor = [UIColor clearColor];
+    label.textColor = [UIColor whiteColor];
+    label.font = [UIFont fontWithName:@"Helvetica-Bold" size:16];
+    label.text = text;
 
-    [self addSubview:self.label];
+    [self addSubview:label];
   }
 
   return self;
 }
+
 
 @end
